f0f5858cd600d165db35d73ca938c25495a58d7a
wangjiepc <wangchenmo1025@gmail.com>
2017-12-24 17:57:52 +0800

    构造MappingHolder,加载类逻辑

--- src/main/java/cn/birdmvc/core/MappingHolder.java
+++ src/main/java/cn/birdmvc/core/MappingHolder.java
@@ -1,8 +1,111 @@
 package cn.birdmvc.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
+ * 访问路径与控制器对象的映射关系持有者
  * @author 王杰
  * @since 2017/12/24
  */
 public class MappingHolder {
+
+    private static final Logger log = LoggerFactory.getLogger(MappingHolder.class);
+
+    //要扫描的包
+    private static String basePakage;
+
+    //扫描文件的基础路径
+    private static String basePath;
+
+    //扫描文件后缀
+    private static String FILE_SUFFFIX = "Controller.class";
+
+    //控制器集合
+    private static List<String> controllerClassNameList;
+
+    //控制器存储类
+    private static Map<String, Object> mapping;
+
+
+    /**
+     * 获取mapping
+     * @param basePakage
+     * @return
+     */
+    public static Map<String, Object> getMapping(String basePakage){
+        MappingHolder.basePakage = basePakage != null?basePakage:"";
+        if(null == mapping)initMapping();
+        return mapping;
+    }
+
+
+
+    /**
+     * 初始化Mapping
+     */
+    private static void initMapping() {
+
+        log.info("init mapping start");
+        log.info("Controller BasePckage:" + basePakage);
+
+        basePakage = basePakage == null ? "" : basePakage;
+        mapping = new HashMap<String, Object>();
+        controllerClassNameList = new ArrayList<String>();
+
+        basePakage = basePakage.replaceAll("\\.", "/");
+
+        try {
+            URI uri = Thread.currentThread().getContextClassLoader().getResource(basePath).toURI();
+            File file = new File(uri);
+            basePath = file.getPath();
+            initControllerClassNameList(file);
+
+            //循环增加Mapping
+            for (String className :
+                    controllerClassNameList) {
+                Class<?> clazz = Class.forName(className);
+                String requestPath = "/"+className.substring(className.lastIndexOf(FILE_SUFFFIX.substring(0,FILE_SUFFFIX.lastIndexOf(".")))).toLowerCase();
+                mapping.put(requestPath,clazz.newInstance());
+                log.info("====>mapping add "+requestPath+"to "+className);
+            }
+
+            log.info("init mapping end");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 初始化ControllerClassName集合
+     *
+     * @param file
+     */
+    private static void initControllerClassNameList(File file) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (File childFile :
+                    files) {
+                initControllerClassNameList(childFile);
+            }
+        } else {
+            String path = file.getPath();
+            if (path.endsWith(FILE_SUFFFIX)) {
+                String absolutePath = file.getAbsolutePath();
+                path = path.substring(basePakage.length(), path.lastIndexOf("."));
+                path = path.replaceAll("\\\\", ".");
+                if (path.startsWith(".")) path = path.substring(1);
+                controllerClassNameList.add(path);
+            }
+        }
+    }
 }
--- src/main/java/cn/birdmvc/web/DispatcherServlet.java
+++ src/main/java/cn/birdmvc/web/DispatcherServlet.java
@@ -1,10 +1,15 @@
 package cn.birdmvc.web;
 
+import cn.birdmvc.core.MappingHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * 请求调度器
@@ -14,14 +19,30 @@ import java.io.IOException;
  */
 public class DispatcherServlet extends HttpServlet{
 
+    private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+    private static final String DEFAULT_VIEWPREFIX = "/WEB-INF/views/";
+    private static final String DEFAULT_VIEWSUFFFIX = ".jsp";
 
+
+    private String viewPrefix;
+    private String viewSuffix;
+    private Map<String, Object> mapping;
     /**
      * 初始化控制器
      * @throws ServletException
      */
     @Override
     public void init() throws ServletException {
+        log.info("DispatcherServlet Init...");
+        //获取初始化参数
+        String basePackage = getInitParameter("basePackage");
+        String viewPrefix = getInitParameter("viewPrefix")  ;
+        String viewSuffix = getInitParameter("viewSuffix");
 
+        this.viewPrefix = viewPrefix!=null?viewPrefix:DEFAULT_VIEWPREFIX;
+        this.viewSuffix = viewSuffix!=null?viewSuffix:DEFAULT_VIEWSUFFFIX;
+        this.mapping = MappingHolder.getMapping(basePackage);
+        log.info("DispatcherServlet Init end");
     }
 
     /**
